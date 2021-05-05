7c3e46c3ed9cb32827a34f39e6aa0668b9f42d1e
Henry Cardenas <83087781+Henry-Esteban-Cardenas-Aleman@users.noreply.github.com>
2021-05-04 19:16:31 -0500

    Implementacion de error al no encontrar la ip

--- content/webIp.py
+++ content/webIp.py
@@ -3,14 +3,23 @@ import os, sys
 
 def dato_erroneo(): 
 
-    if len(sys.argv) != 2 :   
-        print("\n[!] Uso: python3 " + sys.argv[0] + " <direccion-url(sin http)>\n")  
-        sys.exit(1) 
+    '''Ingresar url sin directorios y sin http
+       ejemplo: www.pagina.com
+       no:
+       www.pagina.com/algo/mas
+    '''
 
-if __name__ == '__main__':   
-    
-    try:    
-        url = str(sys.argv[0])    
-        print(f'La ip de < {url} > es {s.gethostbyname(url)}')    
+    if len(sys.argv) != 2 :
+        print("\n[!] Uso: python3 " + sys.argv[0] + " <direccion-url(sin http)>\n")
+        sys.exit(1)
+
+    if s.error:
+        print('Lo sentimos no se encontro la direccion ip ')
+
+if __name__ == '__main__':
+
+    try:
+        url = str(sys.argv[0])
+        print(f'La ip de < {url} > es {s.gethostbyname(url)}')
     except:
 	dato_erroneo()
