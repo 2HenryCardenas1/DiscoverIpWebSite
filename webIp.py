e05908c2e702e36c034411d653652622e9e1e68a
Henry Cardenas <83087781+2HenryCardenas1@users.noreply.github.com>
2021-05-04 22:39:29 -0500

    Update webIp.py

--- webIp.py
+++ webIp.py
@@ -2,27 +2,16 @@
 import socket as s 
 import os, sys
 
-#Autor:Henry Cardenas
-
-def dato_erroneo(): 
-
-    '''Ingresar url sin directorios y sin http
-       ejemplo: www.pagina.com
-       no:
-       www.pagina.com/algo/mas
-    '''
-
+def error(): 
     if len(sys.argv) != 2 :
         print("\n[!] Uso: python3 " + sys.argv[0] + " <direccion-url(sin http)>\n")
         sys.exit(1)
+    elif s.error:
+        print('Lo sentimos no se encontro la direccion ip')
 
-    if s.error:
-        print('Lo sentimos no se encontro la direccion ip ')
-
-if __name__ == '__main__':
-
+if __name__=='__main__':
     try:
         url = str(sys.argv[0])
         print(f'La ip de < {url} > es {s.gethostbyname(url)}')
     except:
-	dato_erroneo()
+	error()
