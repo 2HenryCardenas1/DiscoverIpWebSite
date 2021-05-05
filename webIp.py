dcddda2d8100299a7041ea31a3bd50ca5d3eb118
Henry Cardenas <83087781+2HenryCardenas1@users.noreply.github.com>
2021-05-05 10:17:50 -0500

    Update webIp.py

--- webIp.py
+++ webIp.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-import socket as s 
-import os, sys
+# coding : utf-8
+import socket as s
+import sys
+import time
 
-def error(): 
-    if len(sys.argv) != 2 :
-        print("\n[!] Uso: python3 " + sys.argv[0] + " <direccion-url(sin http)>\n")
+# Autor:Henry Cardenas
+
+def error():
+    if len(sys.argv) != 2:
+        print("\n[!] Uso: python3 " + sys.argv[0] + " <nombre del dominio>\n")
         sys.exit(1)
-    elif s.error:
-        print('Lo sentimos no se encontro la direccion ip')
+    else:
+        print("\n[x] Lo sentimos, no encontramos la ip")
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
+
     try:
-        url = str(sys.argv[0])
-        print(f'La ip de < {url} > es {s.gethostbyname(url)}')
-    except:
-	error()
+        url = str(sys.argv[1])
+        print('\n[!] Descifrando host...')
+        time.sleep(2)
+        print(f'\n\t[*] La ip de < {url} > es {s.gethostbyname(url)}')
+
+    except :
+
+        error()
